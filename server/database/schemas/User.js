675ad56ccde3af83cceb3866b7343939590e7ef2
Daniel Lee <dan47433@gmail.com>
2022-10-27 17:38:49 -0400

    BR-24 User Database Object

--- server/database/schemas/User.js
+++ server/database/schemas/User.js
@@ -1,14 +1,33 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    roles: Array [UUID],
-    history: UUID,
-    username: String,
-    first_name: String,
-    last_name: String,
-    email: String,
-    password: String,
-    status: String
+    roles: [mongoose.SchemaTypes.ObjectId],
+    history: mongoose.SchemaTypes.ObjectId,
+    username: {
+        type: String,
+        required: true,
+    },
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    }
 })
 
 module.exports = mongoose.model("User", userSchema)
--- server/database/schemas/script.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-const User = require("./User")
-
-run()
-async function run() {
-const user = new User({name: "Dan", age: 21 })
-await user.save().then(() => console.log("User Saved"))
-console.log(user)
-} 

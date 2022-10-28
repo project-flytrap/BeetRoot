274b88dc320d90ccd0edb9501c2e37b1e5d9383d
Bryan P <bryanplaza@Bryans-MacBook-Air.local>
2022-10-28 14:28:53 -0400

    Created Library Schema

---
+++ server/database/schemas/Library.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const librarySchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.SchemaTypes.ObjectId,
+    required: true
+  },
+  purchasedSongs: {
+    type: [mongoose.SchemaTypes.ObjectId],
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Library', librarySchema);
